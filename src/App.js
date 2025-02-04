978bd4bc96e83981b0ef712adf24324ab3d73d37
Rodrigo Abreu Amorim <Ramorim13@yahoo.com.br>
2025-02-03 21:27:44 -0300

    Update project

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import Footer from "./components/Footer"
 import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Card from "./components/Card";
+import videos from "./json/db.json";
 
 function App() {
   return (
     <>
       <Header />
-      <Banner  image= "home"/>
+      <Banner image="home" />
       <Container>
         <h2>Geografia</h2>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+
+        <section className="cards">
+          {/* Usando um map para organizar os arquivos do banco de dados de estudo. E falando que para cada video
+          (objeto do banco) vai ser construido um card. E usa-se o key, pois em react utilizando map tem o costume de ser usado para evitar erros no console do desenvolvedor. E tem que ser usado com valores unicos para cada elemento*/}
+          {
+            videos.map(video => <Card id={video.id} key={video.id} />)
+          }
+        </section>
+
       </Container>
       <Footer />
     </>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,13 @@
 import styles from "./Card.module.css";
-function Card () {
+function Card({ id }) {
     return (
-        <section className= {styles.card}>
-            <a 
-                href="https://www.youtube.com/watch?v=B_nq7VTJZds"
+        <section className={styles.card}>
+            <a
+                href={`https://www.youtube.com/watch?v=${id}`}
                 rel="noreferrer noopener"
                 target="_blank"
             >
-                <img src="https://img.youtube.com/vi/B_nq7VTJZds/mqdefault.jpg" alt="Capa"/>
+                <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`} alt="Capa" />
             </a>
         </section>
     );
