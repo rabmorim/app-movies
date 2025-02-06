527c50085ffacad9f9591b7382a757233e46e8fa
rabmorim <ramorim13@yahoo.com.br>
2025-02-06 14:39:44 -0300

    Update study

--- src/pages/Watch/index.js
+++ src/pages/Watch/index.js
@@ -1,11 +1,13 @@
 import styles from "./Watch.module.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import Banner from "../../components/Banner";
 function Watch() {
     return (
         <>
             <Header />
-            <div className= {styles.container}>
+            <Banner image= "assistir" />
+            <div className={styles.container}>
                 <section className={styles.watch}>
                     <iframe width="854" height="480" src="https://www.youtube.com/embed/2y1qW_aH0gQ?si=i2pAc7_wsxbjZ1zn"
                         title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
